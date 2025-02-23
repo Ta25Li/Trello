e270ec93e9c53624a8814211dd97d9dd5c9fcb57
Ta25Li <2885921@gmail.com>
2025-02-23 13:55:25 +0200

    login

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,22 @@
 package tests;
 
-public class LoginTests {
+import dto.User;
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends AppManager {
+
+    @Test
+    public void loginPositive(){
+        User user = User.builder()
+                .email("2885921@gmail.com")
+                .password("QAtesting2025")
+
+                .build();
+
+    new HomePage(getDriver()).clickBtnLogin();
+    new LoginPage(getDriver()).login(user);
+    }
 }
